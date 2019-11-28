d4980adaa2aeed6f1eccdb3135f5becfdd156191
Nick Barlow <nbarlow@turing.ac.uk>
2019-11-28 17:36:12 +0000

    first commit towards a script for getting series of images at different times

---
+++ gee/test_time_series.py
@@ -0,0 +1,51 @@
+"""
+Tests for getting time-series of images
+"""
+
+import pytest
+from get_time_series import *
+
+
+def test_bad_timestring():
+    """
+    Give a nonsensical timestring and check we get an exception
+    """
+    with pytest.raises(RuntimeError) as err:
+        chunks = divide_time_period("blah", "2015-05-12", 12)
+        assert("invalid time strings" in str(err.value))
+
+
+def test_end_before_start():
+    """
+    give an end time earlier than the start time
+    """
+    with pytest.raises(RuntimeError) as err:
+        chunks = divide_time_period("blah", "2015-05-12", 12)
+        assert("end date must be after" in str(err.value))
+
+
+def test_slice_time_period_1():
+    """
+    Try dividing a year into 12.
+    """
+    t1 = "2018-01-01"
+    t2 = "2019-01-01"
+    chunks = divide_time_period(t1, t2, 12)
+    assert(len(chunks)==12)
+    assert(chunks[0][0] == "2018-01-01")
+    assert(chunks[11][1] == "2019-01-01")
+    assert("2018-11" in chunks[11][0])
+
+
+
+def test_slice_time_period_2():
+    """
+    Try dividing a month into 3.
+    """
+    t1 = "2018-01-01"
+    t2 = "2018-02-01"
+    chunks = divide_time_period(t1, t2, 3)
+    assert(len(chunks)==3)
+    assert(chunks[0][0] == "2018-01-01")
+    assert(chunks[1][0] == "2018-01-11")
+    assert(chunks[2][1] == "2018-02-01")
