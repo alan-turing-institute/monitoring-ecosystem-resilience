800b86409d730fa9c2d22fdf6f78659ac3eef1fe
Nick Barlow <nbarlow@turing.ac.uk>
2020-01-20 17:20:52 +0000

    remove obsolete arguments from analyse_gee_data

--- pyveg/scripts/analyse_gee_data.py
+++ pyveg/scripts/analyse_gee_data.py
@@ -37,16 +37,6 @@ from pyveg.src.satellite_data_analysis import get_time_series
 
 
 
-def sanity_check_args(args):
-    """
-    Check that the user has set a self-consistent set of arguments.
-    """
-    if args.coords_point and args.coords_rect:
-      raise RuntimeError("Need to specify ONE of --coords_point or coords_rect")
-    if (args.coords_point or args.coords_rect) and args.input_file:
-      raise RuntimeError("Specify EITHER an input_file OR coords_point or coords_rect")
-
-
 def main():
     """
     use command line arguments to choose images.
@@ -59,8 +49,7 @@ def main():
     parser.add_argument("--end_date",help="YYYY-MM-DD",
                         default="2013-04-01")
     parser.add_argument("--num_time_points",help="Get a time series with this many divisions between start_date and end_date", type=int, default=1)
-    parser.add_argument("--coords_point",help="'long,lat'")
-    parser.add_argument("--coords_rect",help="'long1,lat1,long2,lat2...,...'")
+    parser.add_argument("--coords",help="'long,lat'")
     parser.add_argument("--bands",help="string containing comma-separated list",
                         default="B2,B3,B4,B5,B6,B7")
     parser.add_argument("--region_size", help="size of output region in long/lat", default=0.1, type=float)
@@ -69,11 +58,9 @@ def main():
                         default=".")
     parser.add_argument("--output_suffix",help="end of output filename, including file extension",
                       default="gee_img.png")
-    parser.add_argument("--input_file",help="text file with coordinates, one per line")
     parser.add_argument("--mask_cloud",help="EXPERIMENTAL - apply cloud masking function",action='store_true')
 
     args = parser.parse_args()
-    sanity_check_args(args)
 
     image_coll = args.image_coll
     start_date = args.start_date
@@ -84,18 +71,11 @@ def main():
     region_size = args.region_size
     scale = args.scale
     mask_cloud = True if args.mask_cloud else False
-    input_file = arg.input_file if args.input_file else None
     num_time_points = args.num_time_points
-    if args.coords_point:
-        coords = [float(x) for x in args.coords_point.split(",")]
-    elif args.coords_rect:
-        coords_all = [float(x) for x in args.coords_rect.split(",")]
-        coords = [ [coords_all[2*i],coords_all[2*i+1]] for i in range(int(len(coords_all)/2))]
-    else:
-        coords = None
+    coords = [float(x) for x in args.coords.split(",")]
+
     ##
     get_time_series(num_time_points,
-                    input_file,
                     coords,
                     image_coll,
                     bands,
