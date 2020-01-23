0137486c10be8a65a6d8ba19740b2ec91f7572ee
Nick Barlow <nbarlow@turing.ac.uk>
2020-01-23 17:50:20 +0000

    add docstrings

--- rveg/R/EC_calc.R
+++ rveg/R/EC_calc.R
@@ -4,9 +4,17 @@
 #       Author: Chris A. Boulton
 #       Date: 21th November 2019
 #       email: c.a.boulton@exeter.ac.uk
+#
 
-library(png)
-
+#' Function to find the coordinates of pixels neighbouring a pixel at (x,y) in an m*n array.
+#' Will return an array of eight coordinates (if including diagonal neighbours) or four if not.
+#' @param x The x-coordinate of the pixel of interest
+#' @param y The y-coordinate of the pixel of interest
+#' @param m The size of the array in x-direction.
+#' @param n The size of the array in y-direction.
+#' @param diag Include diagonal neighbours if set to TRUE
+#' @return Array of neighbouring pixel's x-y coordinates.
+#' @export
 get_neighbours <- function(x,y,m,n,diag=TRUE) {
     #find indicdes of surrounding neihgbours of point x,y in an m x n array
     #diagonal neighbours also returned as default
@@ -35,9 +43,12 @@ get_neighbours <- function(x,y,m,n,diag=TRUE) {
 }
 #
 
-
+#' Function that creates a 2-column array of all the edges where neighbour points are both equal to 1
+#' @param pattern A 2D binary array
+#' @return edges A 2-column array of all edges between signal pixels
+#' @export
 make_edges <- function(pattern) {
-    #creates a 2-column array of all the edges where neighbour points are both equal to 1
+
     m <- dim(pattern)[[1]]
     n <- dim(pattern)[[2]]
     edges <- array(0, dim=c(2,2))   #set this up as an array to add to the bottom of and then delete the top two rows at the end
@@ -58,7 +69,10 @@ make_edges <- function(pattern) {
     return(edges)
 }
 
-
+#' Read values from a CSV file into a matrix.
+#' @param filename Full path to csv file
+#' @return matrix containing 1s and 0s.
+#' @export
 read_from_csv <- function(filename) {
     df <- read.csv(filename,header=FALSE)
     mat <- as.matrix(df)
@@ -66,24 +80,39 @@ read_from_csv <- function(filename) {
     return(rescaled_mat)
 }
 
-
+#' Read values from a .png file into a matrix.
+#' @param filename Full path to png file
+#' @return matrix containing 1s and 0s.
+#' @export
 read_from_png <- function(filename) {
-    image <- readPNG(filename)
+    image <- png::readPNG(filename)
     rescaled_image <- apply(image, 1:2, function(x){ if (sum(x) > 0) return(0) else return(1)})
     return(rescaled_image)
 }
 
+
+#' Swap zeros and ones in a matrix
+#' @param pattern matrix containing 0s and 1s
+#' @return matrix containing 1s and 0s.
+#' @export
 invert_pattern <- function(pattern) {
     inverted_pattern <- apply(pattern, 1:2, function(x){ if (x>0) return(0) else return(1)})
     return(inverted_pattern)
 }
 
 
+
+#' Function to calculate the Euler Characteristic for a set of graphs corresponding to different subsets
+#' of signal pixels in an image.
+#' Uses igraph to convert the output from make_edges into a graph object
+#' subgraph centrality is then calculated on this
+#' as default 5% increments are taken where the highest ranked pixels are used to create a subgraph
+#' the number of edges and vertices are then used to calculate the Euler characters as in Mander et al. 2017
+#' @param pattern 2D array of 1s and zeros corresponding to a binary image.
+#' @param inc size of increment in percentage of pixels (ordered by subgraph centrality) in feature vector.
+#' @return EC_vec list of Euler Characteristic values (feature vector).
 calc_EC <- function(pattern, inc=5) {
-    #uses igraph to convert the output from make_edges into a graph object
-    #subgraph centrality is then calculated on this
-    #as default 5% increments are taken where the highest ranked pixels are used to create a subgraph
-    #the number of edges and vertices are then used to calculate the Euler characters as in Mander et al. 2017
+
     require('igraph')
     graph_edges <- make_edges(pattern)
     graph <- graph_from_data_frame(graph_edges, directed = TRUE, vertices = NULL)
