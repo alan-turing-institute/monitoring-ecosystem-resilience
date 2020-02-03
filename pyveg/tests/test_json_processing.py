4ccbd9eeee444223296baafcae401353210960f8
crangelsmith <camila.rangel.smith@gmail.com>
2020-02-03 11:35:03 +0000

    adding tests from #40

--- pyveg/tests/test_json_processing.py
+++ pyveg/tests/test_json_processing.py
@@ -4,11 +4,20 @@ Test the functions in process_network_metrics.py
 
 from pyveg.src.process_network_metrics import *
 
+
+
 def test_process_json_metrics_to_dataframe():
+    test_df = process_json_metrics_to_dataframe(os.path.join(os.path.dirname(__file__),"..","testdata","network_json_data/"))
+    assert (test_df.shape[0]==5)
+    assert (test_df.shape[1]>=8)
 
-    dir_path = os.path.join(os.path.dirname(__file__),"..","testdata","network_json_data")
 
+def test_create_network_figures():
+    dir_path = os.path.join(os.path.dirname(__file__),"..","testdata","network_json_data/")
     test_df = process_json_metrics_to_dataframe(dir_path)
+    create_network_figures(test_df, 'offset50', dir_path, 'test')
 
-    print (test_df.shape)
-    assert (test_df.shape[0]==8)
+    list_png_files = [f for f in os.listdir(dir_path) if (isfile(join(dir_path, f)) and f.endswith(".png"))]
+    list_json_files = [f for f in os.listdir(dir_path) if (isfile(join(dir_path, f)) and f.endswith(".json"))]
+    assert (len(list_png_files)==len(list_json_files))
+
