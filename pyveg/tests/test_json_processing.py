f326f2293360d5baea783b0edaafcb2bd7a392ac
Sam Van Stroud <svanstroud@turing.ac.uk>
2020-03-04 15:08:47 +0000

    fix naming and remove test

--- pyveg/tests/test_json_processing.py
+++ pyveg/tests/test_json_processing.py
@@ -1,8 +1,8 @@
 """
-Test the functions in process_network_metrics.py
+Test the functions in data_analysis_utils.py
 """
 
-from pyveg.src.process_network_metrics import *
+from pyveg.src.data_analysis_utils import *
 
 
 
@@ -11,13 +11,13 @@ def test_read_json_to_dataframe():
     assert (test_df.shape[0] == 7)
     assert (test_df.shape[1] == 90)
 
-
+"""
 def test_create_network_figures():
     dir_path = os.path.join(os.path.dirname(__file__),"..","..","testdata","network_json_data/")
-    test_df = process_json_metrics_to_dataframe(dir_path)
+    test_df = read_json_to_dataframe(os.path.join(os.path.dirname(__file__),"..","..","testdata","test-results_summary.json"))
     create_network_figures(test_df, 'offset50', dir_path, 'test')
 
     list_png_files = [f for f in os.listdir(dir_path) if (isfile(join(dir_path, f)) and f.endswith(".png"))]
     list_json_files = [f for f in os.listdir(dir_path) if (isfile(join(dir_path, f)) and f.endswith(".json"))]
     assert (len(list_png_files)==len(list_json_files))
-
+"""
