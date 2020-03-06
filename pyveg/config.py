6602f878dc1f8680a87310692396f39c63e08619
samvanstroud <sam.van.stroud@cern.ch>
2020-03-06 11:12:50 +0000

    add variable num_days_per_point

--- pyveg/config.py
+++ pyveg/config.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 
 #Define directory to save all outputs
-output_dir = 'RUN1'
+output_dir = 'RUN2'
 
 #coordinates = (27.99,11.29) # initial
 coordinates = (28.37,11.12) # labyrinths
  
 date_range = ('2015-01-01', '2020-01-01')
 
-num_days_per_point = 90
-
-collections_to_use = ['Copernicus','Landsat8', 'ERA5']
+collections_to_use = ['Copernicus','Landsat8', 'ERA5', 'NASA']
 
 do_network_centrality = True
+num_days_per_point = -2
 
 data_collections = {
     'Copernicus' : {
@@ -21,7 +20,8 @@ data_collections = {
         'RGB_bands': ('B4','B3','B2'),
         'NIR_band': 'B8',
         'cloudy_pix_flag': 'CLOUDY_PIXEL_PERCENTAGE',
-        'do_network_centrality': do_network_centrality
+        'do_network_centrality': do_network_centrality,
+        'num_days_per_point': 60
     },
     'Landsat8' : {
         'collection_name': 'LANDSAT/LC08/C01/T1_SR',
@@ -29,7 +29,8 @@ data_collections = {
         'RGB_bands': ('B4','B3','B2'),
         'NIR_band': 'B5',
         'cloudy_pix_flag': 'CLOUD_COVER',
-        'do_network_centrality': do_network_centrality
+        'do_network_centrality': do_network_centrality,
+        'num_days_per_point': 120
     },
     'Landsat5' : {
         'collection_name': 'LANDSAT/LT05/C01/T1_SR',
@@ -56,12 +57,14 @@ data_collections = {
         'collection_name': 'NASA/GPM_L3/IMERG_V06',
         'type': 'weather',
         'precipitation_band': ['precipitationCal'],
+        'num_days_per_point': 30
     },
     'ERA5' : {
-        'collection_name': 'ECMWF/ERA5/DAILY',
+        'collection_name': 'ECMWF/ERA5/MONTHLY',
         'type': 'weather',
         'precipitation_band': ['total_precipitation'],
-        'temperature_band': ['mean_2m_air_temperature']
+        'temperature_band': ['mean_2m_air_temperature'],
+        'num_days_per_point': 30
     }
 }
 
