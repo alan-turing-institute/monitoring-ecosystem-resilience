1408f62463fcb9493b7e0459fe30d989697113f4
Sam Van Stroud <svanstroud@turing.ac.uk>
2020-04-06 11:05:19 +0100

    add check to prevent error in smoothing

--- pyveg/src/data_analysis_utils.py
+++ pyveg/src/data_analysis_utils.py
@@ -375,7 +375,7 @@ def smooth_subimage(df, column='offset50', n=4, it=3):
     ys = df[column]
 
     #num_days_per_timepoint = (xs.iloc[1] - xs.iloc[0]).days
-    frac_data = n / len(ys)
+    frac_data = min(n / len(ys), 1.0)
 
     # perform smoothing
     smoothed_y = lowess(ys, xs, is_sorted=True, return_sorted=False, frac=frac_data, it=it)
