1cbc035d278069a13eea5bce0cb0b66fc9d49be2
nbarlowATI <nbarlow@turing.ac.uk>
2020-04-27 20:25:01 +0100

    update test config file

--- pyveg/config_pipelinetest.py
+++ pyveg/config_pipelinetest.py
@@ -15,7 +15,7 @@ coordinates = (entry.longitude, entry.latitude)
 
 # date range for Copernicus
 #date_range = ['2015-01-01', '2020-01-01']
-date_range = ['2016-01-01', '2016-02-01']
+date_range = ['2016-01-01', '2016-03-01']
 
 #date range for landsat 5
 #date_range = ('1988-01-01', '2003-01-01')
@@ -36,9 +36,11 @@ data_collections = {
         'data_type': 'vegetation',
         'RGB_bands': ['B4','B3','B2'],
         'NIR_band': 'B8',
+        'mask_cloud': True,
+        'cloudy_pix_frac': 50,
         'cloudy_pix_flag': 'CLOUDY_PIXEL_PERCENTAGE',
         'do_network_centrality': do_network_centrality,
-        'num_days_per_point': 30
+        'time_per_point': "1m"
     },
     'Landsat8' : {
         'collection_name': 'LANDSAT/LC08/C01/T1_SR',
@@ -47,7 +49,7 @@ data_collections = {
         'NIR_band': 'B5',
         'cloudy_pix_flag': 'CLOUD_COVER',
         'do_network_centrality': do_network_centrality,
-        'num_days_per_point': 90
+        'time_per_point': "3m"
     },
     'Landsat5' : {
         'collection_name': 'LANDSAT/LT05/C01/T1_SR',
@@ -56,7 +58,7 @@ data_collections = {
         'NIR_band': 'B4',
         'cloudy_pix_flag': 'None',
         'do_network_centrality': do_network_centrality,
-        'num_days_per_point': 90
+        'time_per_point': "3m"
     },
     'Landsat4' : {
         'collection_name': 'LANDSAT/LT04/C01/T1_SR',
@@ -65,14 +67,14 @@ data_collections = {
         'NIR_band': 'B4',
         'cloudy_pix_flag': 'None',
         'do_network_centrality': do_network_centrality,
-        'num_days_per_point': 90
+        'time_per_point': "3m"
     },
     'ERA5' : {
         'collection_name': 'ECMWF/ERA5/MONTHLY',
         'data_type': 'weather',
         'precipitation_band': ['total_precipitation'],
         'temperature_band': ['mean_2m_air_temperature'],
-        'num_days_per_point': 30
+        'time_per_point': "1m"
     }
 }
 
