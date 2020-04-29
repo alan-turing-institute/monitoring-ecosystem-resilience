f8d6eec270ef92b8961b17812e4bd702aac5296c
nbarlowATI <nbarlow@turing.ac.uk>
2020-04-29 10:49:12 +0100

    remove png files after test

--- pyveg/tests/test_data_analysis_utils.py
+++ pyveg/tests/test_data_analysis_utils.py
@@ -2,6 +2,8 @@
 Test the functions in data_analysis_utils.py
 """
 import os
+import shutil
+
 from pyveg.src.data_analysis_utils import *
 
 
@@ -14,8 +16,7 @@ def test_read_json_to_dataframe():
 
 def test_coarse_dataframe():
 
-
-    test_df = variable_read_json_to_dataframe(os.path.join(os.path.dirname(__file__),"..","testdata","network_json_data/test-results-summary.json"))
+    test_df = variable_read_json_to_dataframe(os.path.join(os.path.dirname(__file__),"..","testdata","network_json_data","test-results-summary.json"))
 
     data_df = convert_to_geopandas(test_df['COPERNICUS/S2'])
 
@@ -26,13 +27,12 @@ def test_coarse_dataframe():
     assert (n_blocks==2.0)
 
 
-
-
 def test_create_lat_long_metric_figures():
 
-    dir_path = os.path.join(os.path.dirname(__file__),"..","testdata","network_json_data/")
+    dir_path = os.path.join(os.path.dirname(__file__),"..","testdata","network_json_data","output_png")
+    os.makedirs(dir_path, exist_ok=True)
 
-    test_df = variable_read_json_to_dataframe(os.path.join(os.path.dirname(__file__),"..","testdata","network_json_data/test-results-summary.json"))
+    test_df = variable_read_json_to_dataframe(os.path.join(os.path.dirname(__file__),"..","testdata","network_json_data","test-results-summary.json"))
 
     data_df = convert_to_geopandas(test_df['COPERNICUS/S2'])
 
@@ -46,16 +46,17 @@ def test_create_lat_long_metric_figures():
     len_dates = len(np.unique(data_df['date']))
 
     assert (len(list_png_files)==len_dates)
-
+    # now delete the test png files
+    shutil.rmtree(dir_path, ignore_errors=True)
 
 
 def test_variable_read_json_to_dataframe():
 
-    test_df_dict = variable_read_json_to_dataframe(os.path.join(os.path.dirname(__file__),"..","testdata","network_json_data/test-results-summary.json"))
+    test_df_dict = variable_read_json_to_dataframe(os.path.join(os.path.dirname(__file__),"..","testdata","network_json_data","test-results-summary.json"))
 
     print(test_df_dict.keys())
     dict_len = len(test_df_dict.keys())
     test_df = test_df_dict['COPERNICUS/S2']
-    
+
     assert (test_df.shape == (120,9))
     assert (dict_len == 2)
