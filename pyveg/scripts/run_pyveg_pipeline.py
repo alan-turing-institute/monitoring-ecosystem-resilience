1cb72a14b11f1eb5266e21ce90d858276e65a186
nbarlowATI <nbarlow@turing.ac.uk>
2020-06-01 16:23:46 +0100

    fix to allow running from cached config

--- pyveg/scripts/run_pyveg_pipeline.py
+++ pyveg/scripts/run_pyveg_pipeline.py
@@ -10,6 +10,7 @@ import argparse
 import importlib.util
 import inspect
 from shutil import copyfile
+import re
 
 from pyveg.src.pyveg_pipeline import Pipeline, Sequence
 from pyveg.src.download_modules import VegetationDownloader, WeatherDownloader
@@ -23,10 +24,13 @@ from pyveg.src.processor_modules import (
 from pyveg.src.combiner_modules import VegAndWeatherJsonCombiner
 
 
-def build_pipeline(config_file):
+def build_pipeline(config_file, from_cache=False):
     """
     Load json config and instantiate modules
     """
+    print("Configuring from cached config? {}".format(from_cache))
+    current_time = time.strftime("%Y-%m-%d_%H-%M-%S")
+
     spec = importlib.util.spec_from_file_location("myconfig", config_file)
     config = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(config)
@@ -34,17 +38,28 @@ def build_pipeline(config_file):
     # instantiate and setup the pipeline
     p = Pipeline(config.name)
     p.output_location = config.output_location
-    p.output_location += '__' + time.strftime("%Y-%m-%d_%H-%M-%S")
+    if not from_cache:
+        p.output_location += '__' + current_time
+    else:
+        # use the time from the filename
+        time_match = re.search("([\d]{4}-[\d]{2}-[\d]{2}_[\d]{2}-[\d]{2}-[\d]{2})",
+                               os.path.basename(config_file))
+        if time_match:
+            p.output_location += '__' + time_match.groups()[0]
+        else:
+            print("Wasn't able to infer timestamp from config filename.",
+                  "Will use original output_location from {}.".format(config_file))
     p.output_location_type = config.output_location_type
     p.coords = config.coordinates
     p.date_range = config.date_range
+    if not from_cache:
+        # before we run anything, save the current config to the configs dir
+        config_cache_dir = os.path.join(os.path.dirname(config_file),"cached_config")
+        os.makedirs(config_cache_dir, exist_ok=True)
+        cached_config_file = os.path.basename(config_file)[:-3] + \
+            '__' + current_time + ".py"
 
-    # before we run anything, save the current config to the configs dir
-    configs_dir = os.path.dirname(config_file)
-    cached_config_file = config_file[:-3] + \
-        '__' + time.strftime("%Y-%m-%d_%H-%M-%S") + ".py"
-
-    copyfile(config_file, cached_config_file)
+        copyfile(config_file, os.path.join(config_cache_dir, cached_config_file))
 
     if config.output_location_type=="local" and not os.path.exists(p.output_location):
         os.makedirs(p.output_location, exist_ok=True)
@@ -58,8 +73,9 @@ def build_pipeline(config_file):
             for n, c in inspect.getmembers(sys.modules[__name__]):
                 if n == module_name:
                     module = c()
-                    if n in config.special_config.keys():
-                        module.set_parameters(config.special_config[n])
+                    if "special_config" in vars(config):
+                        if n in config.special_config.keys():
+                            module.set_parameters(config.special_config[n])
                     s += module
         # add the sequence to the pipeline
         p += s
@@ -91,9 +107,11 @@ def configure_and_run_pipeline(pipeline):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--config_file", help="Path to config file", required=True)
+    parser.add_argument("--from_cache", help="Are we using a cached config file to resume an unfinished job?",
+                        action='store_true')
 
     args = parser.parse_args()
-    pipeline = build_pipeline(args.config_file)
+    pipeline = build_pipeline(args.config_file, args.from_cache)
     configure_and_run_pipeline(pipeline)
 
 
