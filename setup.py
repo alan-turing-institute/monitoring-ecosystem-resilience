451e1a688054ee2c1bf09b0713c52eeb0e3f5f17
Sam Van Stroud <svanstroud@turing.ac.uk>
2020-06-18 16:11:55 +0100

    merge setup.py from develop

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("pyveg/requirements.txt", "r") as f:
 
 setup(
     name="pyveg",
-    version="0.0.2",
+    version="1.0.0",
     description="Vegetation patterns study.",
     url="https://github.com/alan-turing-institute/monitoring-ecosystem-resilience",
     author="Nick Barlow, Camila Rangel Smith and Samuel Van Stroud",
@@ -16,11 +16,13 @@ setup(
               "pyveg.scripts",
               "pyveg.configs"],
     install_requires=REQUIRED_PACKAGES,
+    scripts=["pyveg/scripts/batch_commands.sh"],
     entry_points={"console_scripts": [
         "pyveg_calc_EC=pyveg.scripts.calc_euler_characteristic:main",
         "pyveg_gen_pattern=pyveg.scripts.generate_pattern:main",
         "pyveg_gee_download=pyveg.scripts.download_gee_data:main",
         "pyveg_gee_analysis=pyveg.scripts.analyse_gee_data:main",
-        "pyveg_run_pipeline=pyveg.scripts.run_pyveg_pipeline:main"
+        "pyveg_run_pipeline=pyveg.scripts.run_pyveg_pipeline:main",
+        "pyveg_run_module=pyveg.scripts.run_pyveg_module:main"
     ]},
 )
