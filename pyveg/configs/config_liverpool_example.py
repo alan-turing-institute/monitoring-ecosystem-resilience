8e2f5b64bfb5317541842df9df616a1615d00bdb
crangelsmith <crangelsmith@turing.ac.uk>
2022-07-15 14:34:17 +0100

    adding minimal config file for urban grammar

---
+++ pyveg/configs/config_liverpool_example.py
@@ -0,0 +1,42 @@
+from pyveg.coordinates import coordinate_store
+from pyveg.configs.collections import data_collections
+
+name = "liverpool"
+
+# Define location to save all outputs
+output_location = "liverpool-test"
+#output_location_type = "azure"
+output_location_type = "local"
+
+# parse selection. Note (long, lat) GEE convention.
+#entry = coordinate_store.loc[coordinate_id]
+# modify this line to set coords based on entries in `coordinates.py`
+#coordinate_id = "00"
+
+# parse selection. Note (long, lat) GEE convention.
+#entry = coordinate_store.loc[coordinate_id]
+#coordinates = (entry.longitude, entry.latitude)
+
+coordinates = (-2.983333, 53.400002)
+
+date_range = ["2016-01-01", "2021-01-01"]
+
+# From the dictionary entries in data_collections.py, which shall we use
+# (these will become "Sequences")
+collections_to_use = ["Sentinel2"]
+
+# Dictionary defining what Modules should run in each Sequence.
+
+modules_to_use = {
+    "Sentinel2": [
+        "VegetationDownloader",
+        "VegetationImageProcessor",
+    ],
+}
+
+# The following demonstrates how parameters can be set for individual Modules or Sequences:
+special_config = {
+    "Sentinel2": {"time_per_point": "1y", "region_size": 0.1125,
+}     # this is a whole Sequence
+           # and another Module
+}
