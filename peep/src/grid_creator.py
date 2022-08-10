ddbca974a12a0de1775f6033d5e3904632d7a2e0
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2022-08-10 11:20:05 +0100

    Create grids for chips and images of varying dimension

--- peep/src/grid_creator.py
+++ peep/src/grid_creator.py
@@ -7,6 +7,7 @@ import pandas
 import requests
 from icecream import ic
 from shapely.geometry import box
+from shapely.ops import polygonize_full
 
 uk_boundaries_reference_url = "https://github.com/wmgeolab/geoBoundaries/blob/main/releaseData/gbOpen/GBR/ADM0/geoBoundaries-GBR-ADM0_simplified.geojson"
 uk_json = "geoBoundaries-GBR-ADM0_simplified.geojson"
@@ -64,24 +65,74 @@ def get_chip_id(x, y, chip_dimension):
     return "{:0>6}_{:0>7}_{:0>3}".format(x, y, chip_dimension)
 
 
+def coastline_to_coastpoly(path_to_coastline=None):
+    if path_to_coastline is None:
+        path_to_coastline = "zip://strtgi_essh_gb.zip!strtgi_essh_gb/data/coastline.shp"
+
+    coastline = geopandas.read_file(path_to_coastline)
+
+    all_lines = coastline["geometry"].to_list()
+    ic(len(all_lines))
+    result, dangles, cuts, invalids = polygonize_full(all_lines)
+    ic(len(result.geoms))
+    ic(len(result.geoms))
+    ic(len(dangles.geoms))
+    ic(len(cuts.geoms))
+    ic(len(invalids.geoms))
+
+    ic(result)
+    ic(dangles)
+    ic(cuts)
+    ic(invalids)
+
+    polys_gdf = geopandas.GeoDataFrame(
+        {"geometry": result.geoms, "id": True}, crs="EPSG:27700"
+    )
+    ic(polys_gdf)
+
+    polys_gdf.to_file("coast_polys.gpkg", layer="all_polys")
+    polys_gdf = polys_gdf.dissolve()
+    polys_gdf.to_file("coast_polys.gpkg", layer="polys_disolved")
+
+    # gdf['inside'] = gdf['geometry'].apply(lambda shp: shp.intersects(gdfBuffer.dissolve('LAND').iloc[0]['geometry']))
+
+    return polys_gdf
+
+
 if __name__ == "__main__":
-    chip_px_width = 32
+    polys_gdf = coastline_to_coastpoly()
+    # chip_px_width = 32
+    options = {
+        16: "chips_16",
+        32: "chips_32",
+        32 * 16: "images_0512",
+        32 * 32: "images_1024",
+    }
+    options = {32 * 32: "images_1024", 32 * 16: "images_0512"}
 
-    start_time = datetime.now()
+    all_grids = {}
 
-    chips_gdf = create_grid_of_chips(chip_px_width)
+    for chip_px_width, layer_name in options.items():
+        start_time = datetime.now()
 
-    stage1_time = datetime.now()
-    ic(stage1_time - start_time)
-    ic(len(chips_gdf))
+        chips_gdf = create_grid_of_chips(chip_px_width)
 
-    layer_name = f"chips_{chip_px_width:0>2}"
-    chips_gdf.to_parquet(f"{layer_name}.parquet")
+        stage1_time = datetime.now()
+        ic(stage1_time - start_time)
+        ic(len(chips_gdf))
 
-    stage2_time = datetime.now()
-    ic(stage2_time - stage1_time)
+        coast = polys_gdf["geometry"][0]
+        ic(type(coast))
+        chips_gdf["on_land"] = chips_gdf["geometry"].intersects(coast)
+        all_grids[layer_name] = chips_gdf
 
-    chips_gdf.to_file("chips.gpkg", layer=layer_name)
-    stage3_time = datetime.now()
-    ic(stage3_time - stage2_time)
-    ic(stage3_time - start_time)
+        # layer_name = f"chips_{chip_px_width:0>2}"
+        chips_gdf.to_parquet(f"{layer_name}.parquet")
+
+        stage2_time = datetime.now()
+        ic(stage2_time - stage1_time)
+
+        chips_gdf.to_file("chips.gpkg", layer=layer_name)
+        stage3_time = datetime.now()
+        ic(stage3_time - stage2_time)
+        ic(stage3_time - start_time)
